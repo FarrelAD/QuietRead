8a764bece92ae660f7a401683564353d90c00187
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-06-30 13:04:16 +0700

    feat: add tailwindcss integration for styling

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,10 @@ import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
 
+
+/* TailwindCSS */
+import './theme/app.css';
+
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 
@@ -31,16 +35,7 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
-
-/* import '@ionic/react/css/palettes/dark.always.css'; */
-/* import '@ionic/react/css/palettes/dark.class.css'; */
-// import '@ionic/react/css/palettes/dark.system.css';
+/* High contrast theme Ionic */
 import '@ionic/react/css/palettes/high-contrast.system.css';
 import '@ionic/react/css/palettes/high-contrast.always.css';
 
