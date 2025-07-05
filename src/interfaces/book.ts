82f7eb48df3b8688a3d531bce7bbac7f208fa139
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-07-05 17:04:01 +0700

    add new properties [dateAdded, lastRead, progress, isCurrentlyReading] in `Book` interface

--- src/interfaces/book.ts
+++ src/interfaces/book.ts
@@ -2,18 +2,22 @@ export default interface Book {
     id?: number;
     title: string;
     authors: string[];
+    dateAdded: Date;
+    lastRead?: Date;
     publisher: string;
-    publisedDate: Date;
+    publishedDate: Date;
     description: string;
     industryIdentifiers: {
         type: "ISBN_10" | "ISBN_13";
         identifier: string;
     }[],
     pageCount: number;
+    progress: number;
     categories: string[];
     imageLinks: { 
         smallThumbnail: string; 
         thumbnail: string 
     };
+    isCurrentlyReading: boolean;
     language: string;
 }
